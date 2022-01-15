e223752777b47523e982f81e0fb4979df8e39d7f
bonelessnugget <yszeedyt99@gmail.com>
2022-01-15 12:25:55 +0100

    Ejercicio 4

---
+++ Ejercicio1/src/Disco.java
@@ -0,0 +1,33 @@
+
+public class Disco {
+private String titulo;
+private int numeroCanciones;
+private double precio;
+private String fechaCompra;
+public String getTitulo() {
+	return titulo;
+}
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+public int getNumeroCanciones() {
+	return numeroCanciones;
+}
+public void setNumeroCanciones(int numeroCanciones) {
+	this.numeroCanciones = numeroCanciones;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+public String getFechaCompra() {
+	return fechaCompra;
+}
+public void setFechaCompra(String fechaCompra) {
+	this.fechaCompra = fechaCompra;
+}
+
+
+}
---
+++ Ejercicio2/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Empleado empleado = new Empleado();
+		empleado.leerDatos();
+		empleado.verDatos();
+	}
+
+}
